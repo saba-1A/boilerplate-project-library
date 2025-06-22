d29e61c7107e124863c93413527d2f4a9b2cd336
saba-1A <sabafathima789nazar@gmail.com>
2025-06-22 21:49:36 +0530

    Create book.js

---
+++ models/book.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: [String]
+});
+
+module.exports = mongoose.model('Book', bookSchema);
